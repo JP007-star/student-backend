c9265cd4fc8eeefafe83742aeffc63bda25bafe5
JP007-Star <justice_of_peace@yahoo.com>
2023-08-15 22:34:02 +0530

    update & delete

--- src/controller/student.js
+++ src/controller/student.js
@@ -43,3 +43,32 @@ exports.getStudentById = (req, res) => {
       return res.status(400).json({ error: "Params required" });
     }
   };
+
+
+exports.updateStudent=(req,res)=>{
+    Student.updateOne({ _id: req.body.id }, req.body).then((result,error) =>{
+        if (error) {
+            console.error('Error updating document:', error);
+            return res.status(400).json({ error });
+          } else {
+            console.log('Update result:', result);
+            res.status(200).json({ result });
+          }
+      });
+}
+
+exports.deleteStudent=(req,res)=>{
+  console.log(req.query);
+  const { id } = req.query;
+
+  if (id) {
+    Student.deleteOne({ _id: id }).then((product, error) => {
+      if (error) return res.status(400).json({ error });
+      if (product) {
+        res.status(200).json({ product });
+      }
+    });
+  } else {
+    return res.status(400).json({ error: "Params required" });
+  }
+}
--- src/routes/student.js
+++ src/routes/student.js
@@ -1,14 +1,14 @@
 const express=require('express');
 const routes = express.Router()
 const { Router } = require('express');
-const { addStudent, getAllStudents, getStudentById } = require('../controller/student');
+const { addStudent, getAllStudents, getStudentById, updateStudent, deleteStudent } = require('../controller/student');
 
 
 routes.post('/createStudent',addStudent) 
 routes.get('/readAllStudents',getAllStudents)
 routes.get('/studentById',getStudentById)
-// routes.get('/updateStudent/:studentId',getstudentDetailsById)
-// routes.delete( "/deleteStudent", deletestudentById);
+routes.put('/updateStudent',updateStudent)
+routes.delete( "/deleteStudent", deleteStudent);
 
 
 module.exports =routes; 
