d22db77762bf342e6c9fa64651824735c6ce849e
JP007-Star <justice_of_peace@yahoo.com>
2023-08-15 18:54:09 +0530

    read-student

--- src/controller/student.js
+++ src/controller/student.js
@@ -15,3 +15,31 @@ exports.addStudent = async (req, res, next) => {
         res.status(400).json({ error: err })
     })
 }
+
+
+exports.getAllStudents = (req, res) =>  {
+    Student.find({})
+        .then((students, error) => {
+            if (students) return res.status(200).json({ students: students });
+            if (error) {
+
+                return res.status(200).json({ error: error })
+            }
+        })
+}
+
+exports.getStudentById = (req, res) => {
+    console.log(req.query);
+    const { id } = req.query;
+
+    if (id) {
+      Student.findOne({ _id: id }).then((product, error) => {
+        if (error) return res.status(400).json({ error });
+        if (product) {
+          res.status(200).json({ product });
+        }
+      });
+    } else {
+      return res.status(400).json({ error: "Params required" });
+    }
+  };
--- src/routes/student.js
+++ src/routes/student.js
@@ -1,12 +1,12 @@
 const express=require('express');
 const routes = express.Router()
 const { Router } = require('express');
-const { addStudent } = require('../controller/student');
+const { addStudent, getAllStudents, getStudentById } = require('../controller/student');
 
 
 routes.post('/createStudent',addStudent) 
-// routes.get('/readAllStudents',getAllstudent)
-// routes.get('/studentById/:slug',getstudentsBySlug)
+routes.get('/readAllStudents',getAllStudents)
+routes.get('/studentById',getStudentById)
 // routes.get('/updateStudent/:studentId',getstudentDetailsById)
 // routes.delete( "/deleteStudent", deletestudentById);
 
